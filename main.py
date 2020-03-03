3da39307629f7de1cf3172613a9a11beba07f1bb
Coen Onderwater <coenonderwater@gmail.com>
2020-03-03 22:24:05 +0100

    check if the file exists and read the file

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import csv
 
-filename = raw_input("Type the name of the csv file: ") + ".csv"
-print "CSV file: " + filename
 
-def file_exists(filename):
+def check_if_file_exists(full_filename):
     try:
-        f = open()
+        f = open(full_filename)
+        f.close()
+        return True
     except IOError:
         print("File not accessible")
-    finally:
-        f.close()
+        check_if_file_exists()
+
+
+def read_file(full_filename):
+    with open(full_filename, 'r') as csv_file:
+        return csv.reader(csv_file)
+
+
+# Get filename
+filename = raw_input("Type the name of the csv file: ") + ".csv"
+filename = '/files/' + filename
+
+check_if_file_exists(filename)
+data = read_file(filename)
